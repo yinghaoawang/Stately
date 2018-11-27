d8b63f7fccb3a1bcf43d860d554d995ce770cce1
yinghaoawang <yinghao.a.wang@gmail.com>
2018-11-27 15:44:15 -0800

    connects to postgres db

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+const { Client } = require('pg');
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hey');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+console.log(process.env.DATABASE_URL);
+
+client.connect()
+    .then(() => console.log('Connected to the database.'))
+    .catch((err) => console.log(err));
+
+app.get('/aha', (req, res) => {
+    const rando = Math.floor(Math.random() * 100000);
+    res.send(rando);
 });
 
 app.listen(port, () => {
