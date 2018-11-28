dc414a79c1e0f68a2a94610a9f52051c1f8c7ccb
yinghaoawang <yinghao.a.wang@gmail.com>
2018-11-27 16:17:29 -0800

    test

--- index.js
+++ index.js
@@ -17,9 +17,34 @@ client.connect()
     .then(() => console.log('Connected to the database.'))
     .catch((err) => console.log(err));
 
+app.get('/', (req, res) => {
+    const query = 'SELECT * FROM Values';
+
+    client.query(query, function(err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            let str = JSON.stringify(result.rows[0]) + "\n";
+            res.send(str);
+        }
+    });
+});
+
 app.get('/aha', (req, res) => {
     const rando = Math.floor(Math.random() * 100000);
-    res.send(rando);
+
+    const query = `INSERT INTO values (value) VALUES (${rando})`;
+
+    client.query(query, function(err, result) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log(result);
+            let str = JSON.stringify(result.rows[0]) + "\n";
+            res.send(rando + '\n' + str);
+        }
+    });
 });
 
 app.listen(port, () => {
