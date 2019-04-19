09cbde2ac953b9d800c731a77edf95e409e7f603
yinghaoawang <yinghao.a.wang@gmail.com>
2019-04-19 03:02:09 -0700

    baby please

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.all('/*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://endlessarcade.herokuapp.com, http://endlessarcade.herokuapp.com');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
+  res.header('Access-Control-Allow-Origin', 'https://endlessarcade.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', 'http://endlessarcade.herokuapp.com');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
 });
 
 const port = process.env.PORT || 5000;
